b9a873bfd96bd40874ce7fe753c2dae896b0d1bc
Andrei <andrei9669@gmail.com>
2025-11-17 09:12:05 +0200

    add tasks

--- src/routes/page-3/index.tsx
+++ src/routes/page-3/index.tsx
@@ -1,5 +1,8 @@
-import {component$} from "@qwik.dev/core";
+import {$, component$, useTask$} from "@qwik.dev/core";
 
 export default component$(() => {
+    useTask$($(() => {
+        console.log("page-3")
+    }))
     return <div>Page-3</div>
 });
